2ccf3246ed34ea637d9dccafb5e1f89ef0c943c9
G_KAVYASRI <gajulakavyasri5@gmail.com>
2025-01-15 21:51:06 +0530

    `Refactored home page layout and added profile picture and get started button`

--- vewha/lib/pages/home_page.dart
+++ vewha/lib/pages/home_page.dart
@@ -4,55 +4,99 @@ class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
 
   @override
-  State<HomePage> createState() =>  _HomePageState();
+  State<HomePage> createState() => _HomePageState();
 }
 
-class  _HomePageState extends State<HomePage> {
+class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.grey[300],
-      body: SafeArea(
-      child:Column(
-        children: [
-          //app bar
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal:25.0),
-            child: Row
-            (children: [
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text('Hello,',
-                  style: TextStyle(fontSize: 18,fontWeight: FontWeight.bold),
-                  ),
-                  
-                  SizedBox(height:8)
+        backgroundColor: Colors.grey[300],
+        body: SafeArea(
+          child: Column(
+            children: [
+              //app bar
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          'Hello,',
+                          style: TextStyle(
+                              fontSize: 18, fontWeight: FontWeight.bold),
+                        ),
 
-                  Text(
-                    'KavyaSri,',
-                     style: TextStyle(fontSize: 24,fontWeight: FontWeight.bold),
-                  ),
-                ],
+                        SizedBox(height: 8)
+
+                        // Text(
+                        //   'KavyaSri,',
+                        //    style: TextStyle(fontSize: 24,fontWeight: FontWeight.bold),
+                        // ),
+                      ],
+                    ),
+
+                    //profile picture
+                    Container(
+                      padding: EdgeInsets.all(12),
+                      decoration: BoxDecoration(
+                        color: Colors.deepPurple[100],
+                        borderRadius: BorderRadius.circular(12),
+
+                      ) ,
+                      child:Icon(
+                        Icons.person),
+                        
+                    ),
+                  ],
+                ),
               ),
               
 
-              //profile picture
-              Icon(Icons.person),
+              SizedBox(height:25),
+
+              //card--> how do u feel?
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                child: Container(
+                  padding: EdgeInsets.all(20),
+                  decoration: BoxDecoration(color : Colors.pink[100]),
+                  child: Row(children:[
+                    //animation pic
+                    Container(
+                      width: 100,
+                      height: 100,
+                      color: Colors.deepPurple,
+                    ),
+                
+                
+                    //how do u feel + get started button
+                    Column(
+                      children: [
+                      Text('How do you feel?'),
+                      Text('Fill out your medical card right now'),
+                      Container(
+                        child: Center(
+                          child: Text('Get Started'),
+                        ),
+                        )
+                    ],
+                    )
+                  ]),
+                ),
+              ),
+
+
+              //search bar
+
+              //horizontal listview -> categories dentist,surgeon etc..
+
+              //doctor list
             ],
-            ),
           ),
-
-          //card--> how do u feel?
-
-          //search bar
-
-          //horizontal listview -> categories dentist,surgeon etc..
-
-          //doctor list
-        ],
-      ),
-      )
-    );
+        ));
   }
 }
