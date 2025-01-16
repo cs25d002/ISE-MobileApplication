647f1676926334ad92f63e2400662cfb4b9da80c
G_KAVYASRI <gajulakavyasri5@gmail.com>
2025-01-16 11:59:06 +0530

    `Refactored home page layout and widgets`

--- vewha/lib/pages/home_page.dart
+++ vewha/lib/pages/home_page.dart
@@ -45,50 +45,54 @@ class _HomePageState extends State<HomePage> {
                       decoration: BoxDecoration(
                         color: Colors.deepPurple[100],
                         borderRadius: BorderRadius.circular(12),
-
-                      ) ,
-                      child:Icon(
-                        Icons.person),
-                        
+                      ),
+                      child: Icon(Icons.person),
                     ),
                   ],
                 ),
               ),
-              
 
-              SizedBox(height:25),
+              SizedBox(height: 25),
 
               //card--> how do u feel?
               Padding(
                 padding: const EdgeInsets.symmetric(horizontal: 25.0),
                 child: Container(
                   padding: EdgeInsets.all(20),
-                  decoration: BoxDecoration(color : Colors.pink[100]),
-                  child: Row(children:[
+                  decoration: BoxDecoration(color: Colors.pink[100]),
+                  child: Row(children: [
                     //animation pic
                     Container(
                       width: 100,
                       height: 100,
                       color: Colors.deepPurple,
                     ),
-                
-                
-                    //how do u feel + get started button
-                    Column(
-                      children: [
-                      Text('How do you feel?'),
-                      Text('Fill out your medical card right now'),
-                      Container(
-                        child: Center(
-                          child: Text('Get Started'),
-                        ),
-                        )
-                    ],
+                     
+                    SizedBox(
+                      width: 25,
                     )
+                    
+                    //how do u feel + get started button
+                    Expanded(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text('How do you feel?',
+                          style: TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.bold),),
+                          Text('Fill out your medical card right now'),
+                          Container(
+                            child: Center(
+                              child: Text('Get Started'),
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
                   ]),
                 ),
               ),
-
 
               //search bar
 
