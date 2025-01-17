3c74ec6cf16c177c5498a7b4b864a0d96455aaea
G_KAVYASRI <gajulakavyasri5@gmail.com>
2025-01-17 22:22:18 +0530

    `Refactor project to use Flutter Auth package and update dependencies`

--- vewha/lib/main.dart
+++ vewha/lib/main.dart
@@ -1,20 +1,65 @@
-import 'package:flutter/material.dart';
-import 'package:vewha/pages/home_page.dart';
+// import 'package:flutter/material.dart';
+// import 'package:vewha/pages/home_page.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+// void main() {
+//   runApp(const MyApp());
+// }
+
+// class MyApp extends StatelessWidget {
+//   const MyApp({super.key});
+
+// // This widget is the root of your application.
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       debugShowCheckedModeBanner: false,
+//       title: 'VEWHA',
+//       home: HomePage(),
+//     );
+//   }
+// }
+
+import 'package:flutter/material.dart';
+import 'package:flutter_auth/Screens/Welcome/welcome_screen.dart';
+import 'package:flutter_auth/constants.dart';
+
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
 
-// This widget is the root of your application.
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'VEWHA',
-      home: HomePage(),
+      title: 'Flutter Auth',
+      theme: ThemeData(
+          primaryColor: kPrimaryColor,
+          scaffoldBackgroundColor: Colors.white,
+          elevatedButtonTheme: ElevatedButtonThemeData(
+            style: ElevatedButton.styleFrom(
+              elevation: 0,
+              foregroundColor: Colors.white,
+              backgroundColor: kPrimaryColor,
+              shape: const StadiumBorder(),
+              maximumSize: const Size(double.infinity, 56),
+              minimumSize: const Size(double.infinity, 56),
+            ),
+          ),
+          inputDecorationTheme: const InputDecorationTheme(
+            filled: true,
+            fillColor: kPrimaryLightColor,
+            iconColor: kPrimaryColor,
+            prefixIconColor: kPrimaryColor,
+            contentPadding: EdgeInsets.symmetric(
+                horizontal: defaultPadding, vertical: defaultPadding),
+            border: OutlineInputBorder(
+              borderRadius: BorderRadius.all(Radius.circular(30)),
+              borderSide: BorderSide.none,
+            ),
+          )),
+      home: const WelcomeScreen(),
     );
   }
 }
--- vewha/lib/pages/home_page.dart
+++ vewha/lib/pages/home_page.dart
@@ -11,96 +11,97 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: Colors.grey[300],
-        body: SafeArea(
-          child: Column(
-            children: [
-              //app bar
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 25.0),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Column(
+      backgroundColor: Colors.grey[300],
+      body: SafeArea(
+        child: Column(
+          children: [
+            //app bar
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 25.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Hello,',
+                        style: TextStyle(
+                            fontSize: 18, fontWeight: FontWeight.bold),
+                      ),
+                      SizedBox(height: 8),
+                      Text(
+                        'KavyaSri,',
+                        style: TextStyle(
+                            fontSize: 24, fontWeight: FontWeight.bold),
+                      ),
+                    ],
+                  ),
+
+                  //profile picture
+                  Container(
+                    padding: EdgeInsets.all(12),
+                    decoration: BoxDecoration(
+                      color: Colors.deepPurple[100],
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                    child: Icon(Icons.person),
+                  ),
+                ],
+              ),
+            ),
+
+            SizedBox(height: 25),
+
+            //card--> how do u feel?
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 25.0),
+              child: Container(
+                padding: EdgeInsets.all(20),
+                decoration: BoxDecoration(color: Colors.pink[100]),
+                child: Row(children: [
+                  //animation pic
+                  Container(
+                    width: 100,
+                    height: 100,
+                    color: Colors.deepPurple,
+                  ),
+
+                  SizedBox(
+                    width: 25,
+                  ),
+
+                  //how do u feel + get started button
+                  Expanded(
+                    child: Column(
                       crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
                         Text(
-                          'Hello,',
+                          'How do you feel?',
                           style: TextStyle(
-                              fontSize: 18, fontWeight: FontWeight.bold),
+                              fontSize: 16, fontWeight: FontWeight.bold),
                         ),
-
-                        SizedBox(height: 8)
-
-                        // Text(
-                        //   'KavyaSri,',
-                        //    style: TextStyle(fontSize: 24,fontWeight: FontWeight.bold),
-                        // ),
+                        Text('Fill out your medical card right now'),
+                        Container(
+                          child: Center(
+                            child: Text('Get Started'),
+                          ),
+                        ),
                       ],
                     ),
-
-                    //profile picture
-                    Container(
-                      padding: EdgeInsets.all(12),
-                      decoration: BoxDecoration(
-                        color: Colors.deepPurple[100],
-                        borderRadius: BorderRadius.circular(12),
-                      ),
-                      child: Icon(Icons.person),
-                    ),
-                  ],
-                ),
+                  ),
+                ]),
               ),
+            ),
 
-              SizedBox(height: 25),
+            //search bar
 
-              //card--> how do u feel?
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 25.0),
-                child: Container(
-                  padding: EdgeInsets.all(20),
-                  decoration: BoxDecoration(color: Colors.pink[100]),
-                  child: Row(children: [
-                    //animation pic
-                    Container(
-                      width: 100,
-                      height: 100,
-                      color: Colors.deepPurple,
-                    ),
-                     
-                    SizedBox(
-                      width: 25,
-                    )
-                    
-                    //how do u feel + get started button
-                    Expanded(
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text('How do you feel?',
-                          style: TextStyle(
-                            fontSize: 16,
-                            fontWeight: FontWeight.bold),),
-                          Text('Fill out your medical card right now'),
-                          Container(
-                            child: Center(
-                              child: Text('Get Started'),
-                            ),
-                          )
-                        ],
-                      ),
-                    ),
-                  ]),
-                ),
-              ),
+            //horizontal listview -> categories dentist,surgeon etc..
 
-              //search bar
-
-              //horizontal listview -> categories dentist,surgeon etc..
-
-              //doctor list
-            ],
-          ),
-        ));
+            //doctor list
+          ],
+        ),
+      ),
+    );
   }
 }
